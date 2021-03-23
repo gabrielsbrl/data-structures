67f3ebcea4a0b65a52c026e2a41634351843735e
Gabriel Alves <gabrielantonio20111@hotmail.com>
2021-03-23 19:30:40 -0300

    Fix unstasbed files

--- arrayMultiDimension.js
+++ arrayMultiDimension.js
@@ -30,3 +30,16 @@ function printMatrix(matrix) {
 printMatrix(averageTemp);
 
 // example with array of 3x3
+const matrix3x3x3 = [];
+
+for(let i = 0; i < 3; i++) {
+    matrix3x3x3[i] = []; // Each array must be initialized - line
+    for(let j = 0; j < 3; j++) {
+        matrix3x3x3[i][j] = []; // column
+        for(let z = 0; z < 3; z++) {
+            matrix3x3x3[i][j][z] = i + j + z;
+        }
+    }
+}
+
+console.table(matrix3x3x3);
