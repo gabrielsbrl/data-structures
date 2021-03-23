572ca411533d59a6b771cef334aa936b7d7a9085
Gabriel Sobral <gabrielantonio20111@hotmail.com>
2021-03-23 19:51:07 -0300

    Ascending and descending array sorting examples

--- arrayMethods.js
+++ arrayMethods.js
@@ -2,11 +2,17 @@
 
 let exampleArray = [3, 3,4,5,6,7,8,2,34,23,4,754,1];
 
-// sorting items "growing"
+// sorting items "ascending"
 exampleArray.sort((a, b) => {
     if(a < b) return -1;
     if(a == b) return 0;
-    if(a < b) return 1;
+    if(a > b) return 1;
 });
-
-console.log(exampleArray);
+console.log("Ascending: ", exampleArray);
+// sorting items "descending"
+exampleArray.sort((a, b) => {
+    if(a < b) return 1;
+    if(a == b) return 0;
+    if(a > b) return -1;
+});
+console.log("Descending: ", exampleArray);
