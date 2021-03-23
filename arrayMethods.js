e41865aa2d3c9684e3e61e3b84607ce291ea0bd1
Gabriel Sobral <gabrielantonio20111@hotmail.com>
2021-03-23 19:46:39 -0300

    Array methods - sorting method 'growing'

---
+++ arrayMethods.js
@@ -0,0 +1,12 @@
+// some array methods that can be useful
+
+let exampleArray = [3, 3,4,5,6,7,8,2,34,23,4,754,1];
+
+// sorting items "growing"
+exampleArray.sort((a, b) => {
+    if(a < b) return -1;
+    if(a == b) return 0;
+    if(a < b) return 1;
+});
+
+console.log(exampleArray);
