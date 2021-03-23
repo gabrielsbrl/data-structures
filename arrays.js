8970b28485f1408c02b8c1706bc8260bc710fcb8
Gabriel Alves <gabrielantonio20111@hotmail.com>
2021-03-23 16:18:22 -0300

    Representing arrays with fibonacci

---
+++ arrays.js
@@ -0,0 +1,15 @@
+const fibonacci = [];
+
+fibonacci[1] = 1;
+fibonacci[2] = 1;
+
+for(let i = 3; i <= 20; i++) {
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+}
+
+// then prints the whole sequence to the terminal
+/* for(let f of fibonacci) {
+    console.log(f);
+} */
+
+console.table(fibonacci);
