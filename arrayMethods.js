a6370eb19851dc26b1f557181d6103c9bc17f102
gabri <gabrielantonio20111@hotmail.com>
2021-07-24 14:12:28 -0300

    Added the triple equality symbol for each comparison

--- arrayMethods.js
+++ arrayMethods.js
@@ -5,14 +5,14 @@ let exampleArray = [3, 3,4,5,6,7,8,2,34,23,4,754,1];
 // sorting items "ascending"
 exampleArray.sort((a, b) => {
     if(a < b) return -1;
-    if(a == b) return 0;
+    if(a === b) return 0;
     if(a > b) return 1;
 });
 console.log("Ascending: ", exampleArray);
 // sorting items "descending"
 exampleArray.sort((a, b) => {
     if(a < b) return 1;
-    if(a == b) return 0;
+    if(a === b) return 0;
     if(a > b) return -1;
 });
 console.log("Descending: ", exampleArray);
